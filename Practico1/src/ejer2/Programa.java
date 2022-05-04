2679a88250458e1e59bc6225c8af17458a3dc827
Joel Martinez <72528855+Lylat212@users.noreply.github.com>
2022-05-04 14:52:47 -0300

    Add files via upload

--- Practico1/src/ejer2/Programa.java
+++ Practico1/src/ejer2/Programa.java
@@ -1,5 +1,6 @@
 package ejer2;
 import java.util.Scanner;
+import java.util.Random;
 public class Programa {
 	void iniciar(){
 		
@@ -10,23 +11,44 @@ public class Programa {
 		do {
 			num=pedirNumero();
 			resul=compararNumeros(num, valor);
-			System.out.println(resul);
-		}
-			while(resul!=0);
+			//System.out.println(valor);
+			
+			if (resul<0) 
+				System.out.println("El numero es demasiado chico");
+			else if (resul>0) 
+				System.out.println("El numero es demasiado grande");
+			
+			
+			
+			
+		}while(resul!=0);
 			System.out.println("Felicitaciones acertaste!!");
 	}
-			int generarAleatorio() {
+	int generarAleatorio() {
+				Random numero=new Random();
+				int devuelto = numero.nextInt(1000)+1;
+				return devuelto;
 			
+			}
+		
+	int pedirNumero() {
+				int num;
+				Scanner entrada=new Scanner(System.in);
+				System.out.print("Ingrese un Número:");
+				num=entrada.nextInt();
+				return num;
+		
 		}
 		
-			int pedirNumero() {
-			Scanner entrada=new Scanner(System.in);
-			System.out.print("Ingrese un Número:");
-			num=entrada.nextInt();
-		
-		}
-		
-			int compararNumeros(int a, int b) {
+	int compararNumeros(int a, int b) {
+				int resultado;
+				if (a>b)
+					resultado=1;
+				else if (a<b)
+					resultado=-1;
+				else
+					resultado=0;
+				return resultado;
 			
 	}
 }
